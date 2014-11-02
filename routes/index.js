5e2bc8aad34048182b02d72b28306a291c8dd0c2
Evan Yeung <evan.yeung1@gmail.com>
2014-11-02 01:54:18 -0400

    some basic styling done

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var title = 'Jams';
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'MyApp' });
+    res.render('index', { title: title });
 });
 router.get('/:id', function(req, res) {
-    res.render('room', {title: 'MyApp', roomId: req.param('id')});
+    res.render('room', {title: title, roomId: req.param('id')});
 });
 
 module.exports = router;
