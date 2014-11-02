f1b6de2cdf49dbff0a913e62bc26d73cdfff8e88
Evan Yeung <evan.yeung1@gmail.com>
2014-11-02 03:56:15 -0500

    added user list

--- public/javascripts/instruments.js
+++ public/javascripts/instruments.js
@@ -36,6 +36,28 @@ instruments = {
 
         synth.noteOnWithFreq(pitch, 60);
         return synth;
+    },
+
+    violin: function(pitch){
+
+        var synth = T('SynthDef').play();
+
+        synth.def = function(opts) {
+            var osc = T('sin', {freq: opts.freq, mul: 1});
+            var osc1 = T('sin', {freq: opts.freq*2, mul: 0.5});
+            var osc2 = T('sin', {freq: opts.freq*3, mul: 0.33});
+            var osc3 = T('sin', {freq: opts.freq*4, mul: 0.25});
+            var osc4 = T('sin', {freq: opts.freq*5, mul: 0.2});
+            var osc5 = T('sin', {freq: opts.freq*6, mul: 0.16});
+            var osc6 = T('sin', {freq: opts.freq*7, mul: 0.142});
+
+            var env  = T("linen", {a:500, s:100, r:500, lv:0.5},
+                osc1, osc2, osc3, osc4, osc5, osc6);
+            return env.on("ended", opts.doneAction).bang();
+        };
+
+        synth.noteOnWithFreq(pitch, 60);
+        return synth;
     }
 
 };
--- sockets.js
+++ sockets.js
@@ -3,14 +3,30 @@ var app = require('./app');
 var httpServer = require('http').Server(app);
 var io = require('socket.io')(httpServer);
 
+var users = {};
 //sockets
 io.on('connection', function(socket){
     console.log('a user connected');
 
-    socket.on('joinroom', function(username, roomId){
-        socket.username = username;
-        socket.room = roomId;
-        socket.join(roomId);
+    socket.on('joinroom', function(username, room){
+        socket.room = room;
+        socket.join(room);
+
+        if(!users[room]){
+            users[room] = [];
+        }
+        if(users[room].indexOf(username) !== -1){
+            console.log('FAILED');
+            socket.emit('joinedroom', false);
+        }
+        else{
+            console.log('FINE');
+            socket.username = username;
+            users[room].push(username);
+
+            io.sockets.in(room).emit('userchange', users[room]);
+            socket.emit('joinedroom', true);
+        }
     });
 
     socket.on('senddata', function(instrument, data){
@@ -19,6 +35,14 @@ io.on('connection', function(socket){
 
     socket.on('disconnect', function(){
         console.log(socket.username + ' disconnected');
+        if(users[socket.room]){
+            var idx = users[socket.room].indexOf(socket.username);
+            if(idx != -1)
+                users[socket.room].splice(idx, 1);
+                io.sockets.in(room).emit('userchange', users[room]);
+            if(users[socket.room].length === 0)
+                delete users[socket.room];
+        }
     });
 });
 
